df884835ba47a5482aa9748d57e210d325ff04b9
pawni171 <pawni.thakur29@gmail.com>
2025-06-16 10:52:47 +0530

    Added configuration files, documentation, and gitignore

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = 'https://hlxmcnykwjdbpxxyosoc.supabase.co';
+const SUPABASE_SERVICE_ROLE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhseG1jbnlrd2pkYnB4eHlvc29jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODg4MDgyNSwiZXhwIjoyMDY0NDU2ODI1fQ.RGe-KTVUK2F71Uv9EpIl2Nmlj_mJEEmxHN091Fs0a-c';
+
+module.exports = {
+    SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY
+}; 
